90ccc47d25aa27fb363d1c530df079ae5bbedacd
wanchun <wanchunwu101@gmail.com>
2022-05-29 15:48:38 +0800

    [NEW] Add template.py.

---
+++ template.py
@@ -0,0 +1,21 @@
+List = list
+
+
+class Solution:
+    def solveQuestion(self, something: str) -> None:
+        '''
+        :param something:
+        :return:
+        '''
+
+        pass
+
+
+def main():
+    sol = Solution()
+    something = "Some required parameters"
+    sol.solveQuestion(something)
+
+
+if __name__ == '__main__':
+    main()
